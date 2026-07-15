0810b2cb0671c4f49ff2c4b8cc2de0d99ad19285
Viktor.Gladkich <viktorgladkich7@gmail.com>
2026-07-15 21:07:44 +0200

    replace straight quotation marks with typographic quotes in privacy policy page

--- src/app/datenschutz/page.tsx
+++ src/app/datenschutz/page.tsx
@@ -293,8 +293,8 @@ export default function Datenschutz() {
                 Übertragung vertraulicher Inhalte, wie zum Beispiel Anfragen,
                 die Sie an uns als Seitenbetreiber senden, eine SSL- bzw.
                 TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen
-                Sie daran, dass die Adresszeile des Browsers von „http://" auf
-                „https://" wechselt und an dem Schloss-Symbol in Ihrer
+                Sie daran, dass die Adresszeile des Browsers von {"\u201Ehttp://\u201C"} auf
+                {"\u201Ehttps://\u201C"} wechselt und an dem Schloss-Symbol in Ihrer
                 Browserzeile. Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert
                 ist, können die Daten, die Sie an uns übermitteln, nicht von
                 Dritten mitgelesen werden.
